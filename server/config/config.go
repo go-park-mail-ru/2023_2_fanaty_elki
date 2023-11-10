3180853e9fa95710fad1e01eca2e6d3dd9305637
HallowAz <amigo_honor@mail.ru>
2023-11-10 22:57:43 +0300

    fix config

--- server/config/config.go
+++ server/config/config.go
@@ -1,31 +1,34 @@
 package config
 
 import (
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+  "go.uber.org/zap"
+  "go.uber.org/zap/zapcore"
 )
 
 var Cfg = zap.Config{
-	Encoding:         "json",
-	Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
-	OutputPaths:      []string{"../logs/logs.json", "stdout"},
-	ErrorOutputPaths: []string{"../logs/errors.json", "stderr"}, 
-	EncoderConfig: zapcore.EncoderConfig{
-		MessageKey: "message",
-		LevelKey:   "level",
-		TimeKey:    "time",
-		EncodeTime: zapcore.ISO8601TimeEncoder,
-	},
+  Encoding:         "json",
+  Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
+//  OutputPaths:      []string{"../logs/logs.json", "stdout"},
+  OutputPaths:      []string{"stdout"},
+  ErrorOutputPaths: []string{"stderr"},
+  //ErrorOutputPaths: []string{"../logs/errors.json", "stderr"}, 
+  EncoderConfig: zapcore.EncoderConfig{
+    MessageKey: "message",
+    LevelKey:   "level",
+    TimeKey:    "time",
+    EncodeTime: zapcore.ISO8601TimeEncoder,
+  },
 }
 
 var ErrorCfg = zap.Config{
-	Encoding:         "json",
-	Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
-	OutputPaths:      []string{"../logs/errors.json", "stderr"},
-	EncoderConfig: zapcore.EncoderConfig{
-		MessageKey: "message",
-		LevelKey:   "level",
-		TimeKey:    "time",
-		EncodeTime: zapcore.ISO8601TimeEncoder,
-	},
+  Encoding:         "json",
+  Level:            zap.NewAtomicLevelAt(zapcore.InfoLevel),
+  OutputPaths:      []string{"stderr"},
+  //OutputPaths:      []string{"../logs/errors.json", "stderr"},
+  EncoderConfig: zapcore.EncoderConfig{
+    MessageKey: "message",
+    LevelKey:   "level",
+    TimeKey:    "time",
+    EncodeTime: zapcore.ISO8601TimeEncoder,
+  },
 }
