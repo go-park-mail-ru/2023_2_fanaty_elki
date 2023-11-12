09b29d8a35aaad710616feb7090995f8cce7efa7
tomsuliana <ultomskaya@yandex.ru>
2023-11-13 02:07:50 +0300

    Allowed origin changed

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -4,11 +4,11 @@ import (
 	"net/http"
 )
 
-const allowedOrigin = "*"
-//const allowedOrigin = "http://84.23.53.216"
+//const allowedOrigin = "*"
+const allowedOrigin = "http://84.23.53.216"
 
 func CorsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", allowedOrigin)
 		if r.Method == http.MethodOptions {
 			if r.URL.Path != "/api/users" {
@@ -17,9 +17,9 @@ func CorsMiddleware(next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, PATCH, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 			w.WriteHeader(http.StatusNoContent)
-			return  
+			return
 		}
-		if r.Method == http.MethodGet{
+		if r.Method == http.MethodGet {
 			w.Header().Set("Content-Type", "application/json")
 		}
 		next.ServeHTTP(w, r)
@@ -27,7 +27,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 }
 
 func CorsCredentionalsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		next.ServeHTTP(w, r)
 	})
