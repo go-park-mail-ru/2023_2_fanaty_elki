577ffd2c8b80c325fdf0d77ff54008c4f337974a
HallowAz <amigo_honor@mail.ru>
2023-11-25 18:23:23 +0300

    db

--- dev/SQL/csat.sql
+++ dev/SQL/csat.sql
@@ -55,3 +55,6 @@ VALUES(1, 'Насколько удобно вам было регистриро�
 
 insert into QUESTION(QUESTIONNAIRE_ID, text, ANSWER_TYPE)
 VALUES(1, 'Напишите что Вам понравилось, а что нет', 1);
+
+insert into ADMIN(USERNAME, PASSWORD) 
+VALUES("admin", "admin");
