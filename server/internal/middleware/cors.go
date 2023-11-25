736814ea9fee73f80d81d99d1a46859b0c8c4dee
HallowAz <amigo_honor@mail.ru>
2023-11-25 17:59:15 +0300

    cors

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -5,11 +5,14 @@ import (
 )
 
 //const allowedOrigin = "*"
+//const allowedOrigin = "*"
 const allowedOrigin = "http://84.23.53.216"
+const allowedOriginCsat = "http://84.23.53.216:8030"
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", allowedOrigin)
+		w.Header().Add("Access-Control-Allow-Origin", allowedOriginCsat)
 		if r.Method == http.MethodOptions {
 			if r.URL.Path != "/api/users" {
 				w.Header().Add("Access-Control-Allow-Credentials", "true")
