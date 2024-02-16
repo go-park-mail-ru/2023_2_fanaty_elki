6e7c29a3ea2b3193a208bace3e951313b8045d91
Давыдов Максим Сергеевич <davyydovms@student.bmstu.ru>
2024-02-16 11:30:23 +0300

    Updated allowed origins

--- server/internal/middleware/cors.go
+++ server/internal/middleware/cors.go
@@ -4,11 +4,10 @@ import (
 	"net/http"
 )
 
-const allowedOrigin = "http://127.0.0.1:4000"
+// const allowedOrigin = "*"
+const allowedOrigin = "http://localhost:4000"
 
-//const allowedOrigin = "http://prinesy-poday.ru"
-
-//CorsMiddleware provides answer to OPTIONS request and set CORS and CSRF headers. For GET method sets conent-type application/json
+// CorsMiddleware provides answer to OPTIONS request and set CORS and CSRF headers. For GET method sets conent-type application/json
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Origin", allowedOrigin)
@@ -28,7 +27,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-//CorsCredentionalsMiddleware provides header allow credentials
+// CorsCredentionalsMiddleware provides header allow credentials
 func CorsCredentionalsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
